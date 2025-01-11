64681345cb5d5c7fd6541e4ff4194a79748c5df8
ErvinClay13 <ervinclay88@gmail.com>
2025-01-11 01:22:47 -0600

    1st commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const buttons = document.querySelectorAll(".rate");
+const allselected = document.getElementsByClassName("selected");
+const span = document.getElementsByTagName("span")[0];
+let result;
+let section1 = document.querySelector(".section1");
+let section2 = document.querySelector(".section2"); 
+let submit = document.getElementById("submitt");
+let arrowback = document.getElementById("arrowback");
+
+buttons.forEach(button=> {
+button.addEventListener("click", function(){
+
+if (allselected.length > 0 ) {
+  allselected[0].className=allselected[0].className.replace("selected","rate");
+}
+this.className="selected"
+result = button.innerHTML;
+span.innerHTML = result;
+});})
+  
+submit.addEventListener("click", function()  {
+  if (result==null) {alert ("Please, choose the rating!");
+
+  }
+  else {
+    section1.style.display="none";
+    section2.style.display="block";
+    section2.style.display="flex"}
+});
+
+arrowback.addEventListener("click", () =>  {
+  section1.style.display="block";
+  section1.style.display="flex"
+  section2.style.display="none";
+}); 
